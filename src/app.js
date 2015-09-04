58d86eeda4bf2a1d36fbfd433b2f31860e9793d6
Duncan Regan <duncanregan@gmail.com>
2015-09-04 16:11:01 -0400

    send emails through mandrill

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Users from './users';
 import Email from './email';
 import APIKey from './apikey';
-import UserName from './username';
+import FromEmail from './fromemail';
+import EmailSubject from './subject';
 import fetch from './fetch';
 
 class App extends React.Component {
@@ -12,25 +13,47 @@ class App extends React.Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+    var emailSubject = this.refs.emailSubject.state.text;
     var emailText = this.refs.emailText.state.text;
     var emailAddresses = this.refs.users.state.data
       .filter(user => user.checked)
-      .map(user => user.email);
+      .map(user => {return {email: user.email}});
+    var fromEmail = this.refs.fromEmail.state.email;
     var apiKey = this.refs.apiKey.state.key;
-    var dc = apiKey.split('-').pop()
-    var mailChimpApiUrl = `https://${dc}.api.mailchimp.com/3.0/`;
-    var username = this.refs.username.state.name;
+    var madrillUrl = `https://mandrillapp.com/api/1.0/messages/send.json`;
+    var body = {
+      key: apiKey,
+      message: {
+        subject: emailSubject,
+        text: emailText,
+        from_email: fromEmail,
+        to: emailAddresses
+      }
+    }
 
-
-    console.log(fetch(mailChimpApiUrl, {auth: {username: username, password: apiKey}}))
+    fetch(madrillUrl, {method: 'post', body: body})
+      .then(resp => {
+        var failures = resp.filter(email => email.status !== 'sent');
+        if (failures.length) {
+          throw Error('Something went wrong');
+        }
+        else{
+          alert('Your email was sent!');
+        }
+      })
+      .catch(err => {
+        console.error(err);
+        alert('Something went wrong. Please contact support@29.io.');
+      });
 
   }
   render() {
     return(
       <div>
         <form onSubmit={this.handleSubmit}>
+          <FromEmail ref="fromEmail" />
           <APIKey ref="apiKey" />
-          <UserName ref="username" />
+          <EmailSubject ref="emailSubject" />
           <Email ref="emailText" />
           <Users ref="users" />
           <button type="submit">Generate Email</button>
